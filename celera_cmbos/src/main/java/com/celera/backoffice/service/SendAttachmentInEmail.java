22caef2afa738853f76f5e6e9e733dec31a66a7c
lloydchan-celera-markets <lloyd.chan@celera-markets.com>
2017-02-07 13:58:36 +0800

    to email invoices

--- celera_cmbos/src/main/java/com/celera/backoffice/service/SendAttachmentInEmail.java
+++ celera_cmbos/src/main/java/com/celera/backoffice/service/SendAttachmentInEmail.java
@@ -148,9 +148,9 @@ public class SendAttachmentInEmail
 	// public static void main(String[] args)
 	{
 		// Recipient's email ID needs to be mentioned.
-//		String to_2 = "invoices@celera-markets.com";
+		String to_2 = "invoices@celera-markets.com";
 //		String to_2 = "lloyd.chan@celera-markets.com,Guillaume.Cunnington@celera-markets.com";
-		String to_2 = "lloyd.chan@celera-markets.com";
+//		String to_2 = "lloyd.chan@celera-markets.com";
 
 		// Address[] to = null;
 		// try
